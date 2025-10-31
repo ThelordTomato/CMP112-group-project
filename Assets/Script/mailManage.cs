3395b8a204d92b3d0d5413b43d02dbbd6af13c32
Adipa-Siri <2501780@uad.ac.uk>
2025-10-31 18:12:50 +0700

    UI for point count

--- Assets/Script/mailManage.cs
+++ Assets/Script/mailManage.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class mailManage : MonoBehaviour
 {
     public int mail;
+    public Text mailPoint;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +14,6 @@ public class mailManage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mailPoint.text = ": " + mail.ToString();
     }
 }
