b0e166d62ba585f54bfd1d929d34e93d692a9643
Adipa-Siri <2501780@uad.ac.uk>
2025-11-10 11:09:09 +0700

    Fixed error

--- Assets/Script/mailCollect.cs
+++ Assets/Script/mailCollect.cs
@@ -1,20 +1,19 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class mailCollect : MonoBehaviour
 { private BoxCollider2D boxCollider;
     public mailManage mailMan;
-    
+          
 
     private void OnTriggerEnter2D(Collider2D other)
-    {
-         
-        if (other.CompareTag("mail"))
-        {
-            mailMan.mail += 1;
-            Destroy(other.gameObject);
+            {
+                if (other.CompareTag("mail"))
+                {
+                    mailMan.mail += 1;
+                    Destroy(other.gameObject);
 
-        }
-    } 
-}
+                }
+            }
+    }
+
 
